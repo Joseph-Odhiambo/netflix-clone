f4af16dddb38160573f469a76701dd8e89a782d5
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-23 09:56:26 +0300

    Updates

---
+++ precache-manifest.52153f1cbdba3a81257f6c942abd7bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f36b9a00191fb7c01f4d319b0fe7ec",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "18c4320175f4fcca985d",
+    "url": "/netflix-clone/static/css/main.fcef00ab.chunk.css"
+  },
+  {
+    "revision": "a584efa9a15f28832e4b",
+    "url": "/netflix-clone/static/js/2.0082986e.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.0082986e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c4320175f4fcca985d",
+    "url": "/netflix-clone/static/js/main.13196ce1.chunk.js"
+  },
+  {
+    "revision": "6eae586cb898a315916b",
+    "url": "/netflix-clone/static/js/runtime-main.3cb1df92.js"
+  }
+]);
