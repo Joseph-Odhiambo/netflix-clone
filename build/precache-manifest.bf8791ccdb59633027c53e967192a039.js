0aefc6b9525469149c96bc3cd715ac9fce14e528
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-23 09:45:05 +0300

    react netflix

---
+++ build/precache-manifest.bf8791ccdb59633027c53e967192a039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6222dafce98d92801b24b73c37bd3a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95b32dc17894e2086b9",
+    "url": "/static/css/main.fcef00ab.chunk.css"
+  },
+  {
+    "revision": "1d3671a16793152279d0",
+    "url": "/static/js/2.3e19742e.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.3e19742e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95b32dc17894e2086b9",
+    "url": "/static/js/main.4f8a2cf2.chunk.js"
+  },
+  {
+    "revision": "4cfb11e49081e4848833",
+    "url": "/static/js/runtime-main.2d13c343.js"
+  }
+]);
